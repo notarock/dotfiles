ab0c19b63c7d640f41c7fb350f1f63dbc8d7059a
Roch D'Amour <roch.damour@gmail.com>
2021-02-21 22:15:06 -0500

    Nix-doom-emacs

---
+++ users/notarock/doom.d/config.el
@@ -0,0 +1,136 @@
+(setq user-full-name "Roch D'Amour"
+      user-mail-address "roch.damour@gmail.com")
+
+(setq doom-theme 'base16-brewer)
+
+(setq font-family "Essential PragmataPro")
+
+(setq doom-font (font-spec :family font-family :size 22)
+    doom-big-font (font-spec :family font-family :size 34))
+
+(setq display-line-numbers-type t)
+
+(setq fancy-splash-image "/etc/nixos/users/notarock/doom.d/notarock.png")
+
+(setq frame-resize-pixelwise t)
+
+; (feebleline-mode +1)
+(dimmer-configure-magit)
+(dimmer-configure-org)
+(dimmer-mode t)
+(setq highlight-indent-guides-method 'fill)
+(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+
+(add-hook 'prog-mode-hook (lambda ()
+                             (setq show-trailing-whitespace t)))
+(add-hook 'org-mode-hook (lambda ()
+                             (setq show-trailing-whitespace t)))
+(add-hook 'markdown-mode-hook (lambda ()
+                             (setq show-trailing-whitespace t)))
+
+(defun my/set-initial-frame ()
+  "Set initial frame size and position"
+  (let* ((base-factor 0.80)
+         (a-width (* (display-pixel-width) (/ base-factor 2)))
+         (a-height (* (display-pixel-height) base-factor))
+         (a-left (truncate (/ (- (display-pixel-width) a-width) 2)))
+         (a-top (truncate (/ (- (display-pixel-height) a-height) 2))))
+    (set-frame-position (selected-frame) a-left a-top)
+    (set-frame-size (selected-frame) (truncate a-width)  (truncate a-height) t)))
+
+(my/set-initial-frame)
+
+(defun indent-buffer ()
+  "Indent the whole buffer"
+  (interactive)
+  (save-excursion
+    (indent-region (point-min) (point-max) nil)))
+
+(defun insert-random-hash ()
+  (interactive)
+  (insert (string-trim (shell-command-to-string "< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-64};echo;"))))
+
+(evil-ex-define-cmd "q" 'kill-this-buffer)
+(evil-ex-define-cmd "quit" 'evil-quit)
+
+(setq lsp-python-ms-executable (executable-find "python-language-server"))
+
+;; (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+;; (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
+;; (after! flycheck
+  ;; (flycheck-add-mode 'javascript-eslint 'typescript-mode)
+  ;; (flycheck-add-mode 'css-stylelint 'typescript-mode)
+  ;; (add-hook 'typescript-mode-hook (lambda () (flycheck-add-next-checker 'lsp-ui 'javascript-eslint)))
+  ;; (add-hook 'typescript-mode-hook (lambda () (flycheck-add-next-checker 'javascript-eslint 'css-stylelint))))
+
+(add-hook 'js2-mode-hook 'prettier-js-mode)
+(add-hook 'web-mode-hook 'prettier-js-mode)
+
+(map! :map web-mode-map
+      :n "SPC m F" #'eslint-fix)
+
+(map! :map typescript-mode-map
+      :n "SPC m F" #'eslint-fix)
+
+(use-package org-fancy-priorities
+  :hook (org-mode . org-fancy-priorities-mode)
+  :config
+  (setq org-fancy-priorities-list '("■" "■" "■")))
+
+(after! org
+  (map! :map org-mode-map
+        :n "m-j" #'org-metadown
+        :n "m-k" #'org-metaup)
+  (setq ispell-local-dictionary "fr-toutesvariantes"
+        org-directory "~/org/"
+        org-journal-file (concat org-directory "journal.org")
+        org-todos-file (concat org-directory "todos.org")
+        org-capture-templates       (doct '(("Journal" :keys "j"
+                                             :file org-journal-file
+                                             :datetree t
+                                             :todo nil
+                                             :template ("* %^{description}"
+                                                        ":properties:"
+                                                        ":created: %u"
+                                                        ":end:"
+                                                        "%?"))
+                                            ("Todos" :keys "t"
+                                             :file org-todos-file
+                                             :todo-state "TODO"
+                                             :template ("* TODO: %^{description}"
+                                                        ":properties:"
+                                                        ":created: %u"
+                                                        ":end:"))
+                                            ))
+        org-todo-keyword-faces (quote (("todo" :foreground "#ff6347" :weight bold)
+                                       ("done" :foreground "#006400" :weight bold :strike-through t)))
+        org-todo-keywords '((sequence "todo(t)" "done(d)"))
+        org-log-done t))
+
+(after! markdown
+  (setq ispell-local-dictionary "fr-toutesvariantes"))
+
+(map! :ne "C-S-k" #'drag-stuff-up)
+(map! :ne "C-S-j" #'drag-stuff-down)
+(map! :ne "C-S-l" #'drag-stuff-right)
+(map! :ne "C-S-h" #'drag-stuff-left)
+
+(map! :ne "SPC =" #'indent-buffer)
+(map! :ne "SPC #" #'comment-or-uncomment-region)
+
+(map! :ne "SPC j g" #'dumb-jump-go)
+(map! :ne "SPC j b" #'dumb-jump-back)
+
+(map! :ne "SPC i h" #'insert-random-hash)
+
+(map! :ne "SPC w V" (lambda () (interactive)(evil-window-vsplit) (other-window 1)))
+
+(map! [remap org-capture] nil)
+
+;;  (shell-command "pandoc config.org -o README.md")
+
+(global-git-gutter-mode +1)
+
+(global-wakatime-mode +1)
+
+(setq ispell-dictionary "en_US")
---
+++ users/notarock/doom.d/custom.el
@@ -0,0 +1,50 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("5a39d2a29906ab273f7900a2ae843e9aa29ed5d205873e1199af4c9ec921aaab" "840db7f67ce92c39deb38f38fbc5a990b8f89b0f47b77b96d98e4bf400ee590a" "a62f0662e6aa7b05d0b4493a8e245ab31492765561b08192df61c9d1c7e1ddee" "f5f3a6fb685fe5e1587bafd07db3bf25a0655f3ddc579ed9d331b6b19827ea46" "542e6fee85eea8e47243a5647358c344111aa9c04510394720a3108803c8ddd1" "ffac21ab88a0f4603969a24b96993bd73a13fe0989db7ed76d94c305891fad64" "fc7fd2530b82a722ceb5b211f9e732d15ad41d5306c011253a0ba43aaf93dccc" "41eb3fe4c6b80c7ad156a8c52e9dd6093e8856c7bbf2b92cc3a4108ceb385087" "8be07a2c1b3a7300860c7a65c0ad148be6d127671be04d3d2120f1ac541ac103" "87d46d0ad89557c616d04bef34afd191234992c4eb955ff3c60c6aa3afc2e5cc" "eae831de756bb480240479794e85f1da0789c6f2f7746e5cc999370bbc8d9c8a" "df21cdadd3f0648e3106338649d9fea510121807c907e2fd15565dde6409d6e9" "85e6bb2425cbfeed2f2b367246ad11a62fb0f6d525c157038a0d0eaaabc1bfee" "3f67aee8f8d8eedad7f547a346803be4cc47c420602e19d88bdcccc66dba033b" "ecfd522bd04e43c16e58bd8af7991bc9583b8e56286ea0959a428b3d7991bbd8" default))
+ '(fci-rule-color "#555556")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#FD971F"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#A6E22E"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#525254"))
+ '(objed-cursor-color "#E74C3C")
+ '(package-selected-packages '(org-bullets))
+ '(pdf-view-midnight-colors (cons "#F8F8F2" "#272822"))
+ '(rustic-ansi-faces
+   ["#272822" "#E74C3C" "#A6E22E" "#E6DB74" "#268bd2" "#F92660" "#66D9EF" "#F8F8F2"])
+ '(safe-local-variable-values
+   '((ispell-dictionary . "fr-toutesvariantes")
+     (ispell-dictionary . "francais")
+     (ispell-dictionary . "french")))
+ '(vc-annotate-background "#272822")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A6E22E")
+    (cons 40 "#bbdf45")
+    (cons 60 "#d0dd5c")
+    (cons 80 "#E6DB74")
+    (cons 100 "#edc457")
+    (cons 120 "#f5ad3b")
+    (cons 140 "#FD971F")
+    (cons 160 "#fb7134")
+    (cons 180 "#fa4b4a")
+    (cons 200 "#F92660")
+    (cons 220 "#f33254")
+    (cons 240 "#ed3f48")
+    (cons 260 "#E74C3C")
+    (cons 280 "#c14d41")
+    (cons 300 "#9c4f48")
+    (cons 320 "#77504e")
+    (cons 340 "#555556")
+    (cons 360 "#555556")))
+ '(vc-annotate-very-old-color nil)
+ '(wakatime-cli-path "/run/current-system/sw/bin/wakatime")
+ '(wakatime-python-bin nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
