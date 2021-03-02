0d0af6c89fc295f5d9cdba9a2973ce9a3cb4e144
Roch D'Amour <roch.damour@gmail.com>
2021-03-01 23:45:47 -0500

    Removed comment

--- flake.nix
+++ flake.nix
@@ -39,9 +39,5 @@
       };
     };
 
-    # TODO: Enlever cette bouette
-    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    # defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
   };
 }
