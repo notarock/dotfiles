9b4663513b01a724566c11e45d5c983d7db74034
Roch D'Amour <roch.damour@gmail.com>
2023-02-19 03:58:03 -0500

    Enable xmonad with xmobar

--- notarock/extras/xmonad.nix
+++ notarock/extras/xmonad.nix
@@ -10,7 +10,37 @@ in {
     recursive = true;
   };
 
-  services.taffybar.enable = true;
+  programs.xmobar = {
+    enable = true;
+    extraConfig = ''
+      Config { overrideRedirect = False
+              , font     = "xft:Essential PragmataPro-14"
+              , bgColor  = "#5f5f5f"
+              , fgColor  = "#f8f8f2"
+              , position = TopW L 90
+              , commands = [ Run Cpu
+                              [ "-L", "3"
+                              , "-H", "50"
+                              , "--high"  , "red"
+                              , "--normal", "green"
+                              ] 10
+                          , Run Alsa "default" "Master"
+                              [ "--template", "<volumestatus>"
+                              , "--suffix"  , "True"
+                              , "--"
+                              , "--on", ""
+                              ]
+                          , Run Memory ["--template", "Mem: <usedratio>%"] 10
+                          , Run Swap [] 10
+                          , Run Date "%a %Y-%m-%d <fc=#8be9fd>%H:%M</fc>" "date" 10
+                          , Run XMonadLog
+                          ]
+              , sepChar  = "%"
+              , alignSep = "}{"
+              , template = "%XMonadLog% }{ %alsa:default:Master% | %cpu% | %memory% * %swap% | %date% "
+              }
+    '';
+  };
 
   xsession.windowManager.xmonad = {
     enable = true;
